61d5d7dec00980d2ebee74d669a453d0eb471e0b
Jialu Li <jialu_li@g.harvard.edu>
2022-09-14 23:41:03 -0400

    add fun for binary outcomes

--- r/prep_package.r
+++ r/prep_package.r
@@ -27,22 +27,24 @@ plan(multisession, workers =nworkers)
 
 star <- readRDS("/Users/alana/Dropbox/research/research projects/ITR/code/causal-ml/data/star/star.rds")
 
-outcomes <- c("g3tlangss")
 outcomes <- c("g3tlangss",
                 "g3treadss","g3tmathss")
 
-which("g3treadss" == outcomes)
+star_new <- star %>% 
+  mutate(
+    g3tlangss_binary = (g3tlangss > mean(g3tlangss))*1)
+  mutate(g3tlangss_binary = as_factor(g3tlangss_binary))
 
 covariates <-  star %>% dplyr::select(-c(all_of(outcomes),"treatment")) %>% colnames()
 
-fit_star <- run_itr(outcome = outcomes,
+fit_star <- run_itr(outcome = "g3tlangss_binary",
                treatment = "treatment",
                covariates = covariates,
-               data = star,
+               data = star_new,
                algorithms = c(
-                  "causal_forest", 
+                  # "causal_forest", 
                   # "bart",
-                # #  "svm",
+                  # "svm",
                   # "lasso",
                   # "boost", 
                   # "random_forest",
@@ -51,25 +53,43 @@ fit_star <- run_itr(outcome = outcomes,
                plim = 0.2,
                n_folds = 3)
 
-class(fit_star)
+debugonce(compute_qoi)
+debugonce(run_itr)
+
+outcome = vector()
+
+predict_outcome = predict(fit, kyphosis)
+
+convert_outcome <- function(x, predict_outcome){
+  if(predict_outcome[x,1] >= 0.5){
+  outcome[x] = colnames(predict_outcome)[1] %>% as.numeric()
+}else {
+   outcome[x] = colnames(predict_outcome)[2] %>% as.numeric()
+}
+}
+
+sapply(seq(1:nrow(predict_outcome)),convert_outcome, predict_outcome = predict_outcome)
+
 
 
 # get estimates
 summary(fit_star)
 
+
+
 # plot aupec
 plot(x = fit_star, 
-      outcome = outcomes[1],
+      outcome = "g3tlangss_binary",
       treatment = "treatment",
-      data = star, 
+      data = star_new, 
       algorithms = c(
-              "causal_forest",
+              # "causal_forest",
               # "bart",
-              # # "svm",
+              # "svm",
               # "lasso",
               # "boost", 
               # "random_forest",
-              # "bagging", 
+              # "bagging",
               "cart"))
 
 # install the package
