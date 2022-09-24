9d0f5c51afad0490f9b319a4103fbddfb89dabc0
Jialu Li <jialu_li@g.harvard.edu>
2022-09-24 19:29:22 -0400

    up

--- r/prep_package.r
+++ r/prep_package.r
@@ -29,52 +29,37 @@ outcomes <- c("g3tlangss",
                 "g3treadss","g3tmathss")
 
 
-# star_new <- star %>% 
-#   mutate(
-#     g3tlangss_binary = (g3tlangss > mean(g3tlangss))*1) 
+star_new <- star %>% 
+  mutate(
+    g3tlangss_binary = (g3tlangss > mean(g3tlangss))*1) 
 
 # star_new$g3tlangss_binary %>% class
 
 covariates <-  star %>% dplyr::select(-c(all_of(outcomes),"treatment")) %>% colnames()
 
-# binary outcomes
-system.time(
-fit_star <- run_itr(
-               outcome = "g3tlangss_binary",
-               treatment = "treatment",
-               covariates = covariates,
-               data = star_new,
-               algorithms = c(
+##
+## for continous outcomes
+##
+
+algs <- c(
                   # "causal_forest", 
                   # "bartc",
                   # "bart",
                   # "svm",
                   # "lasso",
-                  "boost"), 
-                  # "random_forest",
-                  # "bagging"),
-                  # "cart"),
-               plim = 0.2,
-               n_folds = 2)
-)
+                  # "boost", 
+                  "random_forest")
+                  # "bagging",
+                  # "cart")
 
-# continous outcoms
+# train models
 system.time(
 fit_star <- run_itr(
                outcome = "g3tlangss",
                treatment = "treatment",
                covariates = covariates,
                data = star,
-               algorithms = c(
-                  "causal_forest", 
-                  # "bartc",
-                  "bart",
-                  # "svm",
-                  "lasso",
-                  # "boost", 
-                  # "random_forest",
-                  # "bagging",
-                  "cart"),
+               algorithms = algs,
                plim = 0.2,
                n_folds = 3)
 )
@@ -86,9 +71,9 @@ est_itr <- estimate_itr(
             algorithms = c(
                         "causal_forest", 
                         # "bartc",
-                        "bart",
+                        # "bart",
                         # "svm",
-                        "lasso",
+                        # "lasso",
                         # "boost", 
                         # "random_forest",
                         # "bagging",
@@ -106,29 +91,96 @@ plot(x = est_itr,
       algorithms = c(
                         "causal_forest", 
                         # "bartc",
-                        "bart",
+                        # "bart",
                         # "svm",
-                        "lasso",
+                        # "lasso",
                         # "boost", 
                         # "random_forest",
                         # "bagging",
                         "cart"))
+
+##
+## for binary outcomes
+##
+
+library(randomForest)
+iris %>% 
+   mutate(species_binary = ifelse(Species == "setosa", 1,0)) -> iris_new
+
+mod <- rpart(
+   formula = species_binary ~ Sepal.Length,
+   data = iris_new[1:100,],
+   method = "class")
+
+predict(mod,iris_new[101:150,], type = "prob")[, 2]
+
+predict(mod,iris_new[101:150,], type = "prob")[, 2]  %>% class
+
+
+debugonce(run_itr)          
+debugonce(run_bartmachine)
+debugonce(test_bart)
+
+  ## outcome
+  outcome = testing_data_elements_cart[["data"]][["Y"]]
+
+  if(length(unique(outcome)) > 2){
+      ## predict 
+      Y0t_total = predict(fit_train, newdata=total_data_elements_cart[["data0t"]])
+      Y1t_total = predict(fit_train, newdata=total_data_elements_cart[["data1t"]])
+
+      tau_total=Y1t_total - Y0t_total + runif(n_df,-1e-6,1e-6)
+  }else {
+
+
+#   Y0t_total = predict(
+#     fit_train,
+#     newdata = total_data_elements_bagging[["data0t"]],
+#     type = "prob")[, 2]
+#   Y1t_total = predict(
+#     fit_train,
+#     newdata = total_data_elements_bagging[["data1t"]],
+#     type = "prob")[, 2]
+
+# binary outcomes
+algs <- c(
+                  # "causal_forest", 
+                  # "bartc",
+                  # "bart",
+                  # "svm",
+                  # "lasso",
+                  # "boost", 
+                  # "random_forest",
+                  "bagging",
+                  "cart"
+                  )
+
+system.time(
+fit_star <- run_itr(
+               outcome = "g3tlangss_binary",
+               treatment = "treatment",
+               covariates = covariates,
+               data = star_new,
+               algorithms = algs,
+               plim = 0.2,
+               n_folds = 2))
+
+# compute qoi
+est_itr <- estimate_itr(
+            fit = fit_star,
+            outcome = "g3tlangss_binary",
+            algorithms = algs)            
+
+# get estimates
+summary(est_itr)
 
 # plot aupec for binary outcomes
 plot(x = est_itr, 
       outcome = "g3tlangss_binary",
       treatment = "treatment",
       data = star_new, 
-      algorithms = c(
-              # "causal_forest",
-              # "bartc",
-              # "bart",
-              # "svm",
-              # "lasso",
-              "boost"))
-              # "random_forest",
-              # "bagging",
-              # "cart"))
+      algorithms = algs)
+
 
 # install the package
 devtools::document()
