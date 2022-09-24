2a36e3a7b975da45ea3042d46c7a45d54074fa14
Jialu Li <jialu_li@g.harvard.edu>
2022-09-24 15:33:51 -0400

    up

--- r/prep_package.r
+++ r/prep_package.r
@@ -12,7 +12,6 @@ devtools::load_all()
 # Testing what users will see
 devtools::install()
 
-
 ##
 ## use parallel 
 ## 
@@ -30,55 +29,67 @@ star <- readRDS("/Users/alana/Dropbox/research/research projects/ITR/code/causal
 outcomes <- c("g3tlangss",
                 "g3treadss","g3tmathss")
 
+
 star_new <- star %>% 
   mutate(
-    g3tlangss_binary = (g3tlangss > mean(g3tlangss))*1)
-  mutate(g3tlangss_binary = as_factor(g3tlangss_binary))
+    g3tlangss_binary = (g3tlangss > mean(g3tlangss))*1) 
+
+# star_new$g3tlangss_binary %>% factor(.,levels = c(1,0))
+
+star_new$g3tlangss_binary %>% class
 
 covariates <-  star %>% dplyr::select(-c(all_of(outcomes),"treatment")) %>% colnames()
 
-fit_star <- run_itr(outcome = "g3tlangss",
+system.time(
+fit_star <- run_itr(
+               outcome = "g3tlangss_binary",
                treatment = "treatment",
                covariates = covariates,
-               data = star,
+               data = star_new,
                algorithms = c(
-                  "causal_forest", 
+                  # "causal_forest", 
+                  # "bartc",
                   # "bart",
                   # "svm",
-                  "lasso",
-                  # "boost", 
+                  # "lasso",
+                  "boost"), 
                   # "random_forest",
-                  # "bagging",
-                  "cart"),
+                  # "bagging"),
+                  # "cart"),
                plim = 0.2,
-               n_folds = 5)
+               n_folds = 2)
+)
+debugonce(run_itr)
 
 # get estimates
 summary(fit_star)
 
 # plot aupec
 plot(x = fit_star, 
-      outcome = "g3tlangss",
+      outcome = "g3tlangss_binary",
       treatment = "treatment",
-      data = star, 
+      data = star_new, 
       algorithms = c(
-              "causal_forest",
+              # "causal_forest",
+              # "bartc",
               # "bart",
               # "svm",
-              "lasso",
-              # "boost", 
+              # "lasso",
+              "boost"))
               # "random_forest",
               # "bagging",
-              "cart"))
+              # "cart"))
+
 # install the package
 devtools::document()
 check()
 install()
 
 # import pkg  
-usethis::use_package("cli") 
+usethis::use_package("haven") 
 usethis::use_tidy_description() 
 
 # write readme file
 # usethis::use_data(star, star)
 devtools::build_readme()
+
