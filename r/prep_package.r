97f2ef2f778dacc31c3af6fe23fb3e7a53592145
Jialu Li <jialu_li@g.harvard.edu>
2023-05-06 16:30:04 -0700

    up

--- r/prep_package.r
+++ r/prep_package.r
@@ -14,7 +14,7 @@ devtools::check()
 install()
 
 # import pkg  
-# usethis::use_package("e1071") 
+usethis::use_package("rlearner") 
 usethis::use_tidy_description() 
 
 # write readme file
@@ -45,142 +45,112 @@ covariates <-  star %>% dplyr::select(-c("g3tlangss",
 star_df = star %>% dplyr::select(-c(g3treadss,g3tmathss))
 
 # estimate ITR 
-fit <- run_itr(outcome = outcomes,
-               treatment = treatment,
-               covariates = covariates,
-               data = star_df,
-               algorithms = c("caret"),
-               plim = 0.2,
-               ratio = 0.7,
-              # n_folds = 5,
-              #  trainControl_method = "repeatedcv",
-              trainControl_method = "none",
-              train_method = "gbm")
-              # number = 3,
-              # repeated ten times
-              # repeats = 3)
 
-est <- estimate_itr(fit)
+# User input formula
+user_formula <- as.formula(paste("g3tlangss ~ ", paste0(covariates, collapse = "+"), " + treatment"))
+
+user_formula
+
+# grid search
+gbmGrid <-  expand.grid(interaction.depth = c(1, 5, 9), 
+                        n.trees = (1:30)*50, 
+                        shrinkage = 0.1,
+                        n.minobsinnode = 20)
+
+# fit control
+fitControl = caret::trainControl(method = "none")
+
+fitControl <- caret::trainControl(## 10-fold CV
+                           method = "repeatedcv",
+                           number = 3,
+                           ## repeated ten times
+                           repeats = 3)
+# fit
+fit <- estimate_itr(
+               treatment = "treatment",
+                form = user_formula,
+                # trControl = fitControl,
+               data = star_df,
+               algorithms = c(
+                "lasso", "rf"),
+                # "rlasso", "xlasso", "ulasso", "tlasso"),
+               budget = 0.2,
+               split_ratio = 0.7
+              #  preProcess = c("center", "scale")
+              #  tuneGrid = gbmGrid,
+              #  verbose = FALSE
+               )
+
+# test
+est <- evaluate_itr(fit)
 
 summary(est)
-plot(est)
 
-?train
+# test
+test_out <- test_itr(fit)
 
-?trainControl
+summary(test_out)
 
-fit %>% str
 
-debugonce(run_itr)
-debugonce(itr_single_outcome)
-debugonce(run_caret)
-debugonce(train_caret)
+# plot GATE estimates
+summary(est)$GATE %>% 
+  mutate(group = as_factor(group)) %>%
+  ggplot(., aes(
+    x = group, y = estimate,
+    ymin = lower , ymax = upper, color = algorithm)) +
+  ggdist::geom_pointinterval(
+    width=0.5,    
+    position=position_dodge(0.5),
+    interval_size_range = c(0.8, 1.5),
+    fatten_point = 2.5) +
+  theme_bw() +    
+  # remove grid
+  theme(panel.grid = element_blank(),
+        panel.background = element_blank()) +
+  labs(x = "Group", y = "GATE estimate") +
+  # add dashed line at 0
+  geom_hline(yintercept = 0, linetype = "dashed", color = "#4e4e4e") +
+  scale_color_manual(values = c("#E69F00", "#56B4E9", "#009E73", "#076f00", "#0072B2")) 
+
+ggsave("man/figures/gate.png", width = 5, height = 4)
+
+
+fit$estimates$models$gbm$finalModel$trees
+
 
 debugonce(estimate_itr)
-debugonce(compute_qoi)
+debugonce(itr_single_outcome)
+debugonce(run_rlearner)
+debugonce(train_rlearner)
+debugonce(test_rlearner)
 
 
-##
-## cross validation
-##
+total_data_elements_lasso[["X"]] %>% as.matrix() %>% dim
 
-outcomes <- c("g3tlangss")
-            #     "g3treadss","g3tmathss")
+# get caret model
+fit$estimates$models$rf %>% 
+  ggplot() + theme_bw() 
 
-star_df = star %>% select(-c(g3treadss,g3tmathss)) %>% rename(Y = g3tlangss)
-
-fitControl <- caret::trainControl(method = "none")
+ggsave("man/figures/rf.png", width = 5, height = 4)
 
 
-formula = as.formula(paste("Y ~ (", paste0(covariates, collapse = "+"), ")*treatment"))
-
-fit <- caret::train(formula, data = star_df, 
-                method = "gbm", 
-                trControl = fitControl,
-                ## This last option is actually one
-                ## for gbm() that passes through
-                verbose = FALSE)
-
-test_t0_data = star_df %>% mutate(treatment = 0)
-test_t1_data = star_df %>% mutate(treatment = 1)
-
-total_t0_data = star_df %>% mutate(treatment = 0)
-total_t1_data = star_df %>% mutate(treatment = 1)
-
-
-## predict 
-Y0t_test = predict(
-  fit, 
-  test_t0_data,
-  type = "raw")
-Y1t_test = predict(
-  fit, 
-  test_t1_data,
-  type = "raw")
-
-tau_test = Y1t_test - Y0t_test
-plim = 0.2
-
-## compute quantities of interest 
-That     =  as.numeric(tau_test > 0)
-That_p   = numeric(length(That))
-That_p[sort(tau_test,decreasing =TRUE,index.return=TRUE)$ix[1:(floor(plim*length(tau_test))+1)]] = 1
-
-## output 
-cf_output <- list(
-  tau      = tau_test,
-  tau_cv   = tau_test, 
-  That_cv  = That, 
-  That_pcv = That_p
-  )
-
-
-
-
-
-
-
-
-# estimate ITR 
 set.seed(2021)
-fit <- run_itr(outcome = outcomes,
+fit_cv <- estimate_itr(
                treatment = treatment,
-               covariates = covariates,
-               data = star,
-               algorithms = c("caret"),
-               plim = 0.2,
-               n_folds = 5)
+               form = user_formula,
+               data = star_data,
+               algorithms = c("causal_forest"),
+               trcontrol = fitControl,
+               budget = 0.2,
+               n_folds = 3)
 
-est <- estimate_itr(fit)
+# evaluate ITR 
+est_cv <- evaluate_itr(fit_cv)
 
-summary(est)
-plot(est)
+# plot the AUPEC 
+plot(est_cv)
 
-
-
-debugonce(plot.itr)
-
-?AUPECcv
-
-  graphLabels <- data.frame(
-    type = algorithms,
-    Pval = map(
-      fit[[m]]$AUPEC, ~.x$outputdf) %>%
-      bind_rows() %>%
-      mutate(Pval = paste0("AUPEC = ", round(aupec, 2), " (s.e. = ", round(sd, 2), ")")) %>% pull(Pval))
-
-  Tcv = data %>% pull(treatment) %>% as.numeric()
-  Ycv = data %>% pull(outcome) %>% as.numeric()
-
-  bind_rows(map(fit[[m]]$AUPEC, ~.x$outputdf)) %>% 
-    mutate(type = algorithms) %>%
-    inner_join(bind_rows(
-      map(fit[[m]]$AUPEC, ~.x$outputdf)),
-      by = "type"
-    ) %>%
-    mutate(AUPECmin = aupec.y - 1.96*sd.y,
-          AUPECmax = aupec.y + 1.96*sd.y) %>%
-    rename(aupec = aupec.y) -> data
+ggsave("man/figures/plot_5folds.png", width = 5, height = 4)
 
 
 
@@ -208,7 +178,7 @@ covariates <-  star %>% dplyr::select(-c(all_of(outcomes),"treatment")) %>% coln
 
 
 ##
-## for continous outcomes
+## original algorithms
 ##
 
 algs <- c("causal_forest", 
@@ -226,49 +196,6 @@ algs <- c("causal_forest",
 ggsave("plot_5folds.png")
 saveRDS(est_itr, "est_5folds.rds")
 
-##
-## for binary outcomes
-##
-
-
-# binary outcomes
-algs <- c(
-                  "causal_forest", 
-                  # "bartc",
-                  "bart",
-                  # "svm",
-                  "lasso",
-                  "boost", 
-                  "random_forest",
-                  "bagging",
-                  "cart"
-                  )
-
-system.time(
-fit_star <- run_itr(
-               outcome = "g3tlangss_binary",
-               treatment = "treatment",
-               covariates = covariates,
-               data = star_new,
-               algorithms = algs,
-               plim = 0.2,
-               n_folds = 2))
-
-# compute qoi
-est_itr <- estimate_itr(
-            fit = fit_star,
-            outcome = "g3tlangss_binary",
-            algorithms = algs)            
-
-# get estimates
-summary(est_itr)
-
-# plot aupec for binary outcomes
-plot(x = est_itr, 
-      outcome = "g3tlangss_binary",
-      treatment = "treatment",
-      data = star_new, 
-      algorithms = algs)
 
 
 
